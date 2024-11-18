3b5fcf7aaa697a63de2d1fe5923b2df4c3e6343e
tung <tung.nguyen@rits.center>
2024-11-18 20:45:56 +0700

    Removed research agent code. Focus on priority planning code

--- src/api/agent.py
+++ src/api/agent.py
@@ -5,29 +5,26 @@ Description : Define specified agents working in coordination to answer user's q
 """
 
 import json
-from enum import auto
-from typing import Any, Dict, List, Literal, Optional
+from typing import Any, Dict, Literal
 
 from loguru import logger
-from openai import AsyncAzureOpenAI
 
-from src.utils.core_models.models import Message
 from src.utils.language_models.llms import LLM
 from src.utils.prompting.chunks import Chunks
-from src.utils.prompting.prompt_data import (ConversationalRAGPromptData,
-                                             ResearchPromptData)
-from src.utils.prompting.prompts import (
-    AUGMENT_QUERY_PROMPT_TEMPLATE, DIRECT_ANSWER_PROMPT_TEMPLATE,
-    HYBRID_SEARCH_ANSWER_PROMPT_TEMPLATE, QUERY_ANALYZER_TEMPLATE,
-    RESEARCH_ANSWER_PROMPT_TEMPLATE, RESEARCH_DIRECT_ANSWER_PROMPT_TEMPLATE,
-    REVIEW_CHUNKS_PROMPT_TEMPLATE, REVIEW_INTERNAL_CONTEXT_COMPLETENESS,
-    SEARCH_ANSWER_PROMPT_TEMPLATE, SUMMARIZE_PROMPT_TEMPLATE,
-    TASK_REPHRASE_TEMPLATE)
+from src.utils.prompting.prompt_data import ConversationalRAGPromptData
+from src.utils.prompting.prompts import (AUGMENT_QUERY_PROMPT_TEMPLATE,
+                                         DIRECT_ANSWER_PROMPT_TEMPLATE,
+                                         HYBRID_SEARCH_ANSWER_PROMPT_TEMPLATE,
+                                         QUERY_ANALYZER_TEMPLATE,
+                                         REVIEW_CHUNKS_PROMPT_TEMPLATE,
+                                         REVIEW_INTERNAL_CONTEXT_COMPLETENESS,
+                                         SEARCH_ANSWER_PROMPT_TEMPLATE,
+                                         SUMMARIZE_PROMPT_TEMPLATE)
 from src.utils.reasoning_layers.base_layers import (BaseAgent,
                                                     ExternalContextRetriever,
                                                     InternalContextRetriever)
 
-from .indexing_resource_name import text_index_name
+from .indexing_resource_name import image_index_name, text_index_name
 
 
 class Summarizer(BaseAgent):
@@ -121,46 +118,6 @@ class HybridSearchResponseGenerator(ResponseGenerator):
     async def generate_response(self) -> str:
         self.template = HYBRID_SEARCH_ANSWER_PROMPT_TEMPLATE
         return await self.run()
-
-
-class ResearchResponseGenerator(BaseAgent):
-    """
-    Generates research-based responses using both internal data and external search results.
-
-    Args:
-        llm (LLM): The language model instance.
-        prompt_data (ResearchPromptData): Data for research response generation.
-        stream (bool): Whether to stream responses.
-        generate_config (Dict): Configuration for generating responses.
-
-    Methods:
-        direct_answer: Provides a direct answer to a query.
-        generate_response: Generates a response based on research findings.
-    """
-
-    def __init__(
-        self,
-        llm: LLM,
-        prompt_data: ResearchPromptData,
-        stream: bool,
-        generate_config: Dict,
-    ):
-        super().__init__(
-            llm, prompt_data, stream=stream, generate_config=generate_config
-        )
-
-    async def direct_answer(self) -> str:
-        self.template = RESEARCH_DIRECT_ANSWER_PROMPT_TEMPLATE
-        return await self.run(**self._generate_config)
-
-    def context_found(self):
-        return bool(self.data.chunk_review)
-
-    async def generate_response(self) -> str:
-        if not self.context_found():
-            return "No available information"
-        self.template = RESEARCH_ANSWER_PROMPT_TEMPLATE
-        return await self.run(**self._generate_config)
 
 
 class ContextReviewer(BaseAgent):
@@ -321,56 +278,6 @@ class ChatPriorityPlanner(PriorityPlanningProcessor):
         return response, combined_chunks
 
 
-class Planner:
-    """
-    Orchestrator that uses multiple specialized modules to produce final intelligent answer to ONE query.
-    """
-
-    def __init__(
-        self,
-        client: AsyncAzureOpenAI,
-        stream: bool,
-        generate_config: Dict,
-        search_config: Dict,
-        prompt_data: ConversationalRAGPromptData,
-    ):
-        llm = LLM(client=client)
-        self.prompt_data = prompt_data
-
-        self.query_augmentor = BaseAgent(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            template=AUGMENT_QUERY_PROMPT_TEMPLATE,
-        )
-
-        self.single_query_processor = InternalSingleQueryProcessor(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            search_config=search_config,
-        )
-
-        self.response_generator = ResponseGenerator(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            stream=stream,
-            generate_config=generate_config,
-        )
-        self.stream = stream
-
-    async def run(self):
-        augmented_query = await self.query_augmentor.run()
-        self.prompt_data.update({"query": augmented_query})  # Update prompt data
-
-        decision, chunk_review_data = await self.single_query_processor.process()
-
-        if decision == "answer":
-            response = await self.response_generator.direct_answer()
-        else:
-            response = await self.response_generator.generate_response()
-
-        return response, chunk_review_data
-
-
 class SingleQueryProcessor:
     """
     Processes a single query using either internal or external search tool (but not both), once
@@ -392,6 +299,7 @@ class SingleQueryProcessor:
         self.prompt_data = prompt_data
         self.decision_maker = QueryAnalyzer(llm=llm, prompt_data=prompt_data)
         self.search_type = search_type
+
         if search_type == "external":
             self.context_retriever = ExternalContextRetriever(
                 search_config=search_config
@@ -400,6 +308,7 @@ class SingleQueryProcessor:
             self.context_retriever = InternalContextRetriever(
                 search_config=search_config
             )
+
         self.context_reviewer = ContextReviewer(llm=llm, prompt_data=prompt_data)
 
     async def process(
@@ -422,6 +331,13 @@ class SingleQueryProcessor:
                 self.prompt_data.query,
                 index_name=text_index_name,
             )
+
+            # extend with image search
+            # image_search_result = self.context_retriever.run(
+            #     self.prompt_data.query,
+            #     index_name=image_index_name,
+            # )
+            # filter only images in the found search result
 
         context = Chunks(search_result)
 
@@ -447,128 +363,3 @@ class SingleQueryProcessor:
             )
 
         return decision, chunk_review_data
-
-
-class InternalSingleQueryProcessor:
-    """
-    Processes a single query
-    - decision making (search or not)
-    - search (a strategic searcher depends on whether planning_strategy is priority or greedy)
-    - review
-    """
-
-    def __init__(
-        self,
-        llm: LLM,
-        prompt_data: Any,
-        search_config: Dict[str, Dict[str, Any]],
-    ):
-        self.llm = llm
-        self.prompt_data = prompt_data
-        self.decision_maker = QueryAnalyzer(llm=llm, prompt_data=prompt_data)
-        self.internal_context_retriever = InternalContextRetriever(
-            search_config=search_config.get("internal")
-        )
-        self.external_context_retriever = ExternalContextRetriever(
-            search_config=search_config.get("external")
-        )
-        self.context_reviewer = ContextReviewer(llm=llm, prompt_data=prompt_data)
-
-    async def process(self, decide="auto") -> tuple:
-        if decide == "auto":
-            decision = await self.decision_maker.run()
-            logger.info(f"DECISION = '{decision}'")
-        else:
-            decision = decide  # search or answer
-
-        if decision == "answer":
-            return decision, []
-
-        search_result = self.internal_context_retriever.run(
-            self.prompt_data.query,
-            index_name=text_index_name,
-        )
-
-        context = Chunks(search_result)
-
-        logger.info(
-            f"SEARCH returned {len(context.chunks)} chunks"
-            + "\n".join(context.chunk_ids)
-        )
-
-        # Review chunks
-        self.prompt_data.update({"formatted_context": context.friendly_chunk_view()})
-        chunk_review_str = await self.context_reviewer.run()
-        context.integrate_chunk_review_data(chunk_review_str)
-
-        # Format review result as a string
-        chunk_review_data = context.chunk_review
-
-        self.prompt_data.update({"chunk_review": context.friendly_chunk_review_view()})
-
-        return decision, chunk_review_data
-
-
-class SingleQueryResearcher:
-    """
-    Orchestrator that uses multiple specialized modules to produce final researcher answer on a single query.
-    """
-
-    def __init__(
-        self,
-        llm: LLM,
-        prompt_data: ResearchPromptData,
-        stream: bool,
-        generate_config: Dict,
-        search_config: Dict,
-    ):
-        self.llm = llm
-        self.prompt_data = prompt_data
-
-        self.query_processor = InternalSingleQueryProcessor(
-            llm=llm, prompt_data=self.prompt_data, search_config=search_config
-        )
-        self.response_generator = ResearchResponseGenerator(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            stream=stream,
-            generate_config=generate_config,
-        )
-        self.stream = stream
-
-    async def run(self):
-        decision, chunk_review_data = await self.query_processor.process()
-
-        if decision == "answer":
-            response = await self.response_generator.direct_answer()
-        else:
-            response = await self.response_generator.generate_response()
-
-        return response, chunk_review_data
-
-
-class RephraseResearchPipeline:
-    def __init__(self, llm, prompt_data, generate_config, search_config):
-        self.prompt_data = prompt_data
-        self.rephrase_agent = BaseAgent(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            template=TASK_REPHRASE_TEMPLATE,
-            generate_config=generate_config,
-        )
-        self.research_agent = SingleQueryResearcher(
-            llm=llm,
-            prompt_data=self.prompt_data,
-            stream=False,
-            generate_config=generate_config,
-            search_config=search_config,
-        )
-
-    async def run(self):
-        if not self.rephrase_agent.data.subtasks_results:
-            query = self.rephrase_agent.data.task_description
-        else:
-            query = await self.rephrase_agent.run()
-        self.prompt_data.update({"query": query})
-        answer = self.research_agent.run()
-        return await answer
